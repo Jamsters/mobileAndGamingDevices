1ca56e972cdd13b744d6c56ab180be80b0ad0522
unknown <Jamsters@protonmail.com>
2022-09-29 11:59:02 +0100

    Starting practical

---
+++ mobileGamingProject/app/src/main/java/ac/tees/androidstudiopractice/w9039358/MainActivity.java
@@ -0,0 +1,28 @@
+package ac.tees.androidstudiopractice.w9039358;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "Nice name!";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
+
+
