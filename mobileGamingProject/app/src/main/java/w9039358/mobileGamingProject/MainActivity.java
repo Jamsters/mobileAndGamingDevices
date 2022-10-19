459912b87655564e7fb178949124f188fbc83229
unknown <Jamsters@protonmail.com>
2022-10-19 14:46:19 +0100

    Added new project files for real project.

---
+++ mobileGamingProject/app/src/main/java/w9039358/mobileGamingProject/MainActivity.java
@@ -0,0 +1,21 @@
+package w9039358.mobileGamingProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import w9039358.mobileGamingProject.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ mobileGamingProject/app/src/main/java/w9039358/mobileGamingProject/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package w9039358.mobileGamingProject.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
